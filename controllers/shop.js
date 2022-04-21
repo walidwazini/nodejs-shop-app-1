94bcc8f77b37ae628f8a5fb5d36592712bbdadba
Walid Wazini <wazini25@gmail.com>
2022-04-21 21:55:49 +0800

    updates

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res) => {
+  const productId = req.body.productId
+  console.log(productId)
+  res.redirect('/cart')
+}
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
